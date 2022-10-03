f61bb86a57c8e319930e88cdcce90210eff33f3a
Andy Lichterman <andrewlichterman@northwesternmutual.com>
2022-10-03 16:29:10 +0000

    Update index.js and package-lock.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-  response.send("It worked!")
+  response.send("It worked! my name is andy")
 })
 
 app.listen(3000)
